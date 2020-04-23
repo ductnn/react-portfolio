de60c333cae553d44d88951e394774bae22d252e
DuctnNetwork <trannhuduc531998@gmail.com>
2020-04-23 17:28:12 +0700

    Update resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -13,61 +13,56 @@ class Resume extends Component {
           <Cell col={4}>
             <div style={{textAlign: 'center'}}>
               <img
-                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                src="/img/D_title.png"
                 alt="avatar"
                 style={{height: '200px'}}
                  />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
-            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <h2 style={{paddingTop: '2em'}}>Tran Duc</h2>
+            <h4 style={{color: 'grey'}}>Developer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
+            <p>Ha Noi, Viet Nam</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>+84 XXX-XXX</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
+            <p>ductn@devtel.com</p>
             <h5>Web</h5>
-            <p>mywebsite.com</p>
+            <p>ductnn.github.io</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
 
 
-            <Education
-              startYear={2002}
-              endYear={2006}
-              schoolName="My University"
-              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-               />
+              <Education
+                startYear={2016}
+                endYear={2021}
+                schoolName="My Posts and Telecommunications Institute of Technology"
+                schoolDescription="Major: Electronics and Telecommunications"
+                />
 
-               <Education
-                 startYear={2007}
-                 endYear={2009}
-                 schoolName="My 2nd University"
-                 schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                  />
-                <hr style={{borderTop: '3px solid #e22947'}} />
+            <hr style={{borderTop: '3px solid #e22947'}} />
 
-              <h2>Experience</h2>
-
-            <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
-              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              />
+            <h2>Experience</h2>
 
               <Experience
-                startYear={2012}
-                endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                startYear={2016}
+                endYear= "present"
+                jobName="Network & System"
+                jobDescription="Routing & Switching, Network Automation, Docker & Linux Command"
                 />
+
+                <Experience
+                  startYear={2016}
+                  endYear= "present"
+                  jobName="Coding"
+                  jobDescription="IoT: Smart Farm, Web Developer"
+                  />
+
               <hr style={{borderTop: '3px solid #e22947'}} />
               <h2>Skills</h2>
               <Skills
